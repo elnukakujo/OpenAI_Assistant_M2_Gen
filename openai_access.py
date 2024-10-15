bc204de498dd39bde4ab93933e4b537d4a23a7c9
sopigggia <zhangtongfei1016@gmail.com>
2024-10-15 17:05:05 -0400

    added dotenv for secret keys management

--- openai_access.py
+++ openai_access.py
@@ -1,10 +1,6 @@
 from openai import OpenAI
-
-def get_api_key(path):
-    key_file = open(path, "r")
-    api_key = key_file.read()
-    key_file.close()
-    return api_key
+from dotenv import load_dotenv
+import os
 
 def get_prompt(path):
     key_file = open(path, "r")
@@ -18,7 +14,8 @@ def write_output(path, output):
     key_file.close()
     return True
 
-client = OpenAI(api_key=get_api_key("openai_key.txt"))
+load_dotenv()
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 response = client.chat.completions.create(model="gpt-3.5-turbo", messages=[
     {"role": "system", "content": "You are a helpful assistant."},
     {"role": "user", "content": get_prompt("prompt.txt")}
