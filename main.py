1be394bff55af373789c8279260a98f3ce3971b9
elnukakujo <jagernoe1s3@hotmail.com>
2024-11-15 13:42:51 -0500

    More docu for info on the user settings in main

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-import os
-
 from prepare_prompt import prepare_prompt
 from call_llm import call_llm
 from convert_ecore import convert_ecore
 
 def main(example_method, DSL_name, llm_idx):
+    """
+    Do not edit this function.
+    Main function to prepare a prompt, call a LLM API, and convert the output to Ecore format.
+
+    Args:
+        example_method (str): The example method to be used in the prompt preparation corresponding to the explanations passed to the LLM in data/Example/<>.txt.
+        DSL_name (str): The name of the Domain-Specific Language (DSL) to be used corresponding to the directory with the domain informations.
+        llm_idx (int): The index of the LLM model to be called. The two current options are:
+        {
+            0: "gpt-3.5-turbo",
+            1: "gpt-4"
+        }.
+
+    Returns:
+        None
+    """
     print("Preparing the prompt...")
     prompt = prepare_prompt(example_method, DSL_name)
     print("----------------------")
@@ -15,7 +29,9 @@ def main(example_method, DSL_name, llm_idx):
     convert_ecore(DSL_name)
     
 if __name__ == "__main__":
+    # Edit your parameters here
     example_method = "3shot"
     DSL_name = "HBMS"
     llm_idx = 0
+    # Edit your parameters here
     main(example_method, DSL_name, llm_idx)
